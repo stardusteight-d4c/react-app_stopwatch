ef9b76f01e525eda49fedf29c5b999482abe4e9c
Gabriel Sena <87643260+stardusteight-d4c@users.noreply.github.com>
2022-05-17 11:58:54 -0300

    configurando form

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,20 @@ import Botao from '../Botao';
 import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "0:00:00"
+  }
+
+  enviarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log('state:', this.state);
+    
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.enviarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -14,6 +25,11 @@ class Formulario extends React.Component {
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={evento => this.setState({
+              ...this.state,
+              tarefa: evento.target.value
+            })}
             placeholder="O que você quer estudar"
             required
           />
@@ -26,9 +42,14 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={evento => this.setState({
+              ...this.state,
+              tempo: evento.target.value
+            })}
             id="tempo"
             min="00:00:00"
-            max="01:30:00"
+            max="06:00:00"
             required
           />
         </div>
